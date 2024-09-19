b88531ad12c0db60694f03a504e09a11cf0184e1
ybk-tomoya <ybk_tomoya@keio.jp>
2024-09-19 12:02:14 +0900

    first commit

---
+++ sayHello.js
@@ -0,0 +1,6 @@
+function sayHello(firstname) {
+    console.log('Hello ' + firstname);
+}
+var firstname = 'World';
+var age = 20;
+sayHello(age);
---
+++ sayHello.ts
@@ -0,0 +1,8 @@
+// Testcode
+function sayHello(firstname: string) {
+    console.log('Hello ' + firstname);
+}
+
+let firstname: string = 'World';
+let age :number = 20;
+sayHello(firstname);
